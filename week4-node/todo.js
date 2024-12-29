99815652a8d49f374aa55b8c4d5a09e2f5dc0353
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2024-12-28 20:02:45 -0700

    do ti

--- week4-node/todo.js
+++ week4-node/todo.js
@@ -3,8 +3,45 @@ const { Command } = require("commander");
 const program = new Command();
 
 const filepath = "./todos.json";
+program.command('mark')
+  .description('Marks task as done in the todo list and updates the todos.json file, make sure to use quotes with task with spaces')
+  .argument('<task>', 'task to mark as done')
+  .action((task) => {
+    task = task.trim().toLowerCase();
+    fs.readFile(filepath, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      let todos = [];
+      try {
+        todos = JSON.parse(data);
+      } catch (err) {
+        todos = [];
+      }
+    let found = false;
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].title.trim().toLowerCase() === task) {
+          todos[i].done = true;
+          found = true;
+          break;
+        }
+      }
+      if (!found){
+        console.log('Task not found');  
+      }
+      fs.writeFile(filepath, JSON.stringify(todos), (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (found){
+          console.log(`Task marked: ${task}`);
+          }
+        }
+      });
+    });
+  });
 program.command("add")
-  .description("CLI to do file-based tasks")
+  .description("Adds tasks to the todo list and stores them in todos.json, make sure to use quotes for task with spaces")
   .argument("<task>", "task to add")
   .action((task) => {
       fs.readFile(filepath, "utf8", (err, data) => {
@@ -28,19 +65,42 @@ program.command("add")
       });
   });
 program.command('delete')
-  .description('Count the number of lines in a file')
-  .argument('<file>', 'file to count')
-  .action((file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
+  .description('Deletes task from the todo list and updates the todos.json file, make sure to use quotes with task with spaces')
+  .argument('<task>', 'task to delete')
+  .action((task) => {
+    task = task.trim().toLowerCase();
+    fs.readFile(filepath, 'utf8', (err, data) => {
       if (err) {
         console.log(err);
-      } else {
-        const lines = data.split('\n').length;
-        console.log(`There are ${lines} lines in ${file}`);
       }
+      let todos = [];
+      try {
+        todos = JSON.parse(data);
+      } catch (err) {
+        todos = [];
+      }
+      let found = false;
+      for (let i = 0; i < todos.length; i++) {
+        if (todos[i].title.trim().toLowerCase() === task) {
+          todos.splice(i, 1);
+          found = true;
+          break;
+        }
+      }
+      if (!found){
+        console.log('Task not found');  
+      }
+      fs.writeFile(filepath, JSON.stringify(todos), (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (found){
+          console.log(`Task deleted: ${task}`);
+          }
+        }
+      });
     });
   });
-  program.command('mark')
 function main() {
   program.parse();
 }
