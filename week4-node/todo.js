ae152cfbeb7abbc16663859b3a9feaa7c108202a
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2024-12-28 19:35:58 -0700

    implemetation of todo CLI

--- week4-node/todo.js
+++ week4-node/todo.js
@@ -1,95 +1,47 @@
-const fs = require('fs');
-const { Command } = require('commander');
+const fs = require("fs");
+const { Command } = require("commander");
 const program = new Command();
 
-const FILE_PATH = 'tasks.json';
-
-// Helper to read tasks from the file
-function readTasks() {
-  try {
-    const data = fs.readFileSync(FILE_PATH, 'utf8');
-    return JSON.parse(data);
-  } catch (err) {
-    // Return an empty array if file doesn't exist
-    return [];
-  }
-}
-
-// Helper to write tasks to the file
-function writeTasks(tasks) {
-  fs.writeFileSync(FILE_PATH, JSON.stringify(tasks, null, 2));
-}
-
-// Command: Add a new task
-program
-  .command('add')
-  .description('Add a new task to the to-do list')
-  .argument('<task>', 'Task description')
+const filepath = "./todos.json";
+program.command("add")
+  .description("CLI to do file-based tasks")
+  .argument("<task>", "task to add")
   .action((task) => {
-    const tasks = readTasks();
-    tasks.push({ id: tasks.length + 1, task, done: false });
-    writeTasks(tasks);
-    console.log(`Task added: "${task}"`);
-  });
-
-// Command: List all tasks
-program
-  .command('list')
-  .description('List all tasks')
-  .action(() => {
-    const tasks = readTasks();
-    if (tasks.length === 0) {
-      console.log('No tasks found.');
-    } else {
-      console.log('Your tasks:');
-      tasks.forEach((t) => {
-        console.log(`[${t.done ? 'x' : ' '}] ${t.id}: ${t.task}`);
+      fs.readFile(filepath, "utf8", (err, data) => {
+          if (err) {
+              console.log(err);
+          }
+          let todos = [];
+          try {
+              todos = JSON.parse(data);
+          } catch (err) {
+              todos = [];
+          }
+          todos.push({ title: task, done: false });
+          fs.writeFile(filepath, JSON.stringify(todos), (err) => {
+              if (err) {
+                  console.log(err);
+              } else {
+                  console.log(`Task added: ${task}`);
+              }
+          });
       });
-    }
   });
-
-// Command: Mark a task as done
-program
-  .command('done')
-  .description('Mark a task as done')
-  .argument('<id>', 'Task ID')
-  .action((id) => {
-    const tasks = readTasks();
-    const task = tasks.find((t) => t.id === parseInt(id, 10));
-    if (!task) {
-      console.log(`Task with ID ${id} not found.`);
-    } else {
-      task.done = true;
-      writeTasks(tasks);
-      console.log(`Task "${task.task}" marked as done.`);
-    }
+program.command('delete')
+  .description('Count the number of lines in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const lines = data.split('\n').length;
+        console.log(`There are ${lines} lines in ${file}`);
+      }
+    });
   });
-
-// Command: Remove a task
-program
-  .command('remove')
-  .description('Remove a task by its ID')
-  .argument('<id>', 'Task ID')
-  .action((id) => {
-    let tasks = readTasks();
-    const taskIndex = tasks.findIndex((t) => t.id === parseInt(id, 10));
-    if (taskIndex === -1) {
-      console.log(`Task with ID ${id} not found.`);
-    } else {
-      const removedTask = tasks.splice(taskIndex, 1);
-      writeTasks(tasks);
-      console.log(`Task removed: "${removedTask[0].task}"`);
-    }
-  });
-
-// Command: Clear all tasks
-program
-  .command('clear')
-  .description('Clear all tasks')
-  .action(() => {
-    writeTasks([]);
-    console.log('All tasks cleared.');
-  });
-
-// Parse the command-line arguments
-program.parse();
+  program.command('mark')
+function main() {
+  program.parse();
+}
+main();
