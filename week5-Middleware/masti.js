b74c1b5478ad330e6a940cdaa4812f66394a62c0
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2024-12-30 15:10:28 -0700

    map functionality

--- week5-Middleware/masti.js
+++ week5-Middleware/masti.js
@@ -3,3 +3,12 @@
 //arrow function implementation
 const sum = (a,b) => a + b;
 console.log(sum(2,3)); // 5
+ //map implementation
+const arr = [1,2,3,4,5];
+
+// Kind of like linear alg where u create a transformation :)
+function transform(x){
+    return x * 2;
+}  
+const arr2 = arr.map(transform);
+console.log(arr2); // [2,4,6,8,10]
