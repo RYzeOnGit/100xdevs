2d851935545f7f21a749db99eb890635d1eb79e1
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2024-12-29 21:24:19 -0700

    implementation of app.get

--- week4-express/lol.js
+++ week4-express/lol.js
@@ -1,19 +1,39 @@
-// Playing with postman
+// FileSystem based todo with express functionality and testing with postman
 const fs = require('fs');
 const express = require('express');
 const app = express();
+
+const filepath = "./todos.json";
 //Route handler
 app.get('/', (req, res) => {
   const n = req.query.n;
+  n = n.trim().toLowerCase();
+  fs.readFile(filepath, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    let todos = [];
+    try {
+      todos = JSON.parse(data);
+    } catch (err) {
+      todos = [];
+    }
+    if (!todos){
+        res.send('No tasks found');
+    }
+    else{
+        res.send(todos);
+    }
   const sum =   n*(n+1)/2;
   res.send(`Sum of 1 to ${n} is ${sum}`);
 })
 app.post('/', (req, res) => {
     res.send('POST request to the homepage');
   })
-app.get('/wasd', (req, res) => {
-    const n = req.query.n;
-    const sum =   n*n
-    res.send(`Square of ${n} is ${sum}`);
+app.put('/', (req, res) => {
+    res.send('PUT request to the homepage');
   })
-app.listen(3000)
+app.delete('/', (req, res) => {
+    res.send('DELETE request to the homepage');
+  })
+app.listen(3000); //start server
