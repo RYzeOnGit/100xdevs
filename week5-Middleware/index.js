4291f36612396bfbc0590e8fa46550e7f64b5438
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2024-12-30 05:07:35 -0700

    middleware implemetation

--- week5-Middleware/index.js
+++ week5-Middleware/index.js
@@ -2,19 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.get('/sum/:a/:b', function(req, res,){
+let request_count = 0;
+
+function requests(req, res, next){
+    request_count++;
+    console.log(request_count);
+    next();
+}
+function sumhandler(req, res){
     const a = req.params.a;
     const b = req.params.b;
     res.json({
         result: parseInt(a) + parseInt(b)
     })
-})
-
-app.get('/multiply', function(req, res) {
+}
+function multiplyhandler(req, res){
     const a = req.query.a;
     const b = req.query.b;
     res.json({
         result: parseInt(a) * parseInt(b)
     })
-})
+}
+app.get('/sum/:a/:b', requests, sumhandler);
+
+app.get('/multiply', requests, multiplyhandler); 
+
 app.listen(3000);
