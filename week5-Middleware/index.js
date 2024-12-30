6f71dcd0a6dc6b4b9f30c288690359ee4f6cf831
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2024-12-30 04:25:08 -0700

    addition functionality

---
+++ week5-Middleware/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/sum', function(req, res,){
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        result: parseInt(a) + parseInt(b)
+    })
+})
+
+app.get('/multiply', function(req, res) {
+})
