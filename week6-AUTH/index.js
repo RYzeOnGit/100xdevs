2ea99bc1f9d71220957393c4460076731631b329
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2024-12-31 02:34:07 -0700

    signin, signup token work with express

--- week6-AUTH/index.js
+++ week6-AUTH/index.js
@@ -2,12 +2,60 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // for parsing application/json middleware function
+
+const users = [];
+// Generation of a random token for the user
+function generateToken(){
+    return Math.random().toString(36);
+}
 // signing up
 app.post('/signup', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    if(users.find(user => user.username === username)) {
+        res.json({
+            message: "User already exists!"
+        });
+        return;
+    }
+    users.push({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: "You are signed in!"
+    })
+    console.log(users);
 });
 
 //signing in
 app.post('/signin', function(req, res) {
-
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    let foundUser = null;
+    for(let i = 0; i < users.length; i++) {
+        if(users[i].username === username && users[i].password === password) {
+            foundUser = users[i];
+            break;
+        }
+    }
+    if (foundUser) {
+        const token = generateToken();
+        res.json({
+            message: "You are signed in!",
+            token: token
+        });
+    }
+    else {
+        res.status(403).send({
+            message: "Invalid username or password!"
+        });
+    }
+    console.log(users);
 });
+
+app.listen(3000);
