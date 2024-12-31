ed73e0837ae856e8aa0d5264137f65b3a8337a41
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2024-12-31 01:41:25 -0700

    skeleton structure

---
+++ week6-AUTH/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+// signing up
+app.post('/signup', function(req, res) {
+
+});
+
+//signing in
+app.post('/signin', function(req, res) {
+
+});
