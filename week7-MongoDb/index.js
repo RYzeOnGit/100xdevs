723d86748f23f1640d534f40edc7c6ef1efee61b
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-01-01 17:32:49 -0700

    jwt and signin implementation:

--- week7-MongoDb/index.js
+++ week7-MongoDb/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { UserModel, TodoModel } = require('./db');
 
+const jwt = require('jsonwebtoken');
 const app = express();
 
 app.use(express.json());
+jwt_secret = "menepyaarkiya@8008s.com";
 
 app.post("/signup", async function(req, res) {
    const email = req.body.email;
@@ -22,7 +24,27 @@ app.post("/signup", async function(req, res) {
 });
 
 app.post("/signin", function(req, res) {
-    // Your code here
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = UserModel.findOne({
+        email: email,
+        password: password
+    });
+
+    if (user) {
+        const token = jwt.sign({
+            id: user._id
+        }, jwt_secret);
+        res.json({
+            token: token
+            });
+        }
+    else {
+        res.status(403).json({
+            message: "Invalid credentials"
+        });
+    }
 });
 
 app.post("/todo", function(req, res) {
