3095051ff07036776380806256db9dc4696d54df
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-01-01 17:07:23 -0700

    skeleton structure

---
+++ week7-MongoDb/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.post("/signup", function(req, res) {
+  // Your code here
+});
+
+app.post("/signin", function(req, res) {
+    // Your code here
+});
+
+app.post("/todo", function(req, res) {
+    // Your code here
+});
+
+app.post("/todos", function(req, res) {
+// Your code here
+});
+
+app.listen(3000);
