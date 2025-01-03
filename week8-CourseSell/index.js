118005d8d7239813e24dfa34ec6df1426e941203
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-01-03 04:29:24 -0700

    skeleton structure

---
+++ week8-CourseSell/index.js
@@ -0,0 +1,13 @@
+//  Course selling app backend
+const express = require('express');
+const app = express();
+
+app.post('/api/courses', (req, res) => {
+});
+app.post('/api/courses', (req, res) => {
+});
+app.post('/api/courses', (req, res) => {
+});
+app.get('/api/courses', (req, res) => {
+});
+app.listen(3000);
