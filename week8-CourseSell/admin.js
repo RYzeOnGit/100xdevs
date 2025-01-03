6f716f91353c4c062767c3589d0b7425fca2833c
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-01-03 05:04:41 -0700

    adding skeleton code for admin route

---
+++ week8-CourseSell/admin.js
@@ -0,0 +1,12 @@
+const {Router} = require('express');
+const adminRouter = Router();
+
+adminRouter.post('/admin/signin', (req, res) => {
+    
+});
+
+adminRouter.post('/api/courses', (req, res) => {        
+});
+
+adminRouter.get('/api/courses', (req, res) => {       
+});
--- week8-CourseSell/index.js
+++ week8-CourseSell/index.js
@@ -1,7 +1,12 @@
 //  Course selling app backend
 const express = require('express');
+const { userRouter } = require('./user');
+const { courseRouter } = require('./course');
 const app = express();
 
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/course", courseRouter);
 
 
 app.listen(3000);
