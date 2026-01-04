a655bd615544d2fdcfba780d7386ab079ed8c5a2
Ryan <ryan.chattopadhyay1@gmail.com>
2026-01-04 06:27:07 -0700

    back to basics hehe

---
+++ week1-web-dev/beginner.js
@@ -0,0 +1,9 @@
+// Interpreted language - Python, JavaScript, Ruby, PHP, etc. Code runs line by line.\
+// issue with interpreted languages is that they are slower than compiled languages and more prone to runtime errors.
+
+// Dynamically typed language - JavaScript, Python, Ruby, etc. Variables are not declared with a type and can be changed.
+
+var a = 1;
+console.log(a);
+var a = "Hello";
+console.log(a);
